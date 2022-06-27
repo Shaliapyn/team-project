741e01fedfda31328b4958d9fa4d41976c20b8bb
Ihor Shalapa <igorshalapa@gmail.com>
2022-06-27 22:56:32 +0300

    hide sidebar

--- src/layout/Sidebar/Sidebar.js
+++ src/layout/Sidebar/Sidebar.js
@@ -6,6 +6,7 @@ import { SidebarData } from './SidebarData'
 import styles from 'assets/scss/sidebar.module.scss'
 import { SmileSvg, ArrowSvg } from 'assets/svg/svg-icons'
 import MenuContext from 'context/MenuContext'
+import { memberState } from 'store/slices/memberSlice'
 
 const Sidebar = () => {
   const [isSidebarBig, setIsSidebarBig] = useState(true)
@@ -18,47 +19,49 @@ const Sidebar = () => {
   const hideSideBarClass = isMenuCheked ? `${styles.menuToggle}` : null
 
   const role = useSelector((state) => state.member.member.role)
+  const member = useSelector(memberState)
   const [currentUser, setCurrentUser] = useState(role)
 
   useEffect(() => {
     setCurrentUser(role)
   }, [role])
-
   return (
     <>
-      <nav className={`${sidebarClasses} ${hideSideBarClass}`}>
-        <div className={styles.roleBlock}>
-          {isSidebarBig && <SmileSvg />}
-          <h2 className={styles.roleText}>{currentUser}</h2>
-        </div>
-        <hr className={styles.horizonLine} />
-        <ul style={{ paddingLeft: '0px' }}>
-          {!!currentUser &&
-            SidebarData.map((el, key) => (
-              <Link
-                onClick={() => setIsMenuChecked(false)}
-                key={key}
-                className={`text-white ${styles.Link}`}
-                to={el.link}
-              >
-                {el.rolesAccess.includes(currentUser) && (
-                  <li>
-                    <a className={sidebarLiClasses} href="">
-                      <div className={sidebarIconLiClasses}>{el.icon}</div>
-                      {isSidebarBig && <div>{el.title}</div>}
-                    </a>
-                  </li>
-                )}
-              </Link>
-            ))}
-        </ul>
-        <hr className={styles.horizonLine} />
-        {!isMenuCheked && (
-          <div className={sidebarArrowClasses}>
-            <ArrowSvg onClick={() => setIsSidebarBig(!isSidebarBig)} />
+      {member.email ? (
+        <nav className={`${sidebarClasses} ${hideSideBarClass}`}>
+          <div className={styles.roleBlock}>
+            {isSidebarBig && <SmileSvg />}
+            <h2 className={styles.roleText}>{currentUser}</h2>
           </div>
-        )}
-      </nav>
+          <hr className={styles.horizonLine} />
+          <ul style={{ paddingLeft: '0px' }}>
+            {!!currentUser &&
+              SidebarData.map((el, key) => (
+                <Link
+                  onClick={() => setIsMenuChecked(false)}
+                  key={key}
+                  className={`text-white ${styles.Link}`}
+                  to={el.link}
+                >
+                  {el.rolesAccess.includes(currentUser) && (
+                    <li>
+                      <a className={sidebarLiClasses} href="">
+                        <div className={sidebarIconLiClasses}>{el.icon}</div>
+                        {isSidebarBig && <div>{el.title}</div>}
+                      </a>
+                    </li>
+                  )}
+                </Link>
+              ))}
+          </ul>
+          <hr className={styles.horizonLine} />
+          {!isMenuCheked && (
+            <div className={sidebarArrowClasses}>
+              <ArrowSvg onClick={() => setIsSidebarBig(!isSidebarBig)} />
+            </div>
+          )}
+        </nav>
+      ) : null}
     </>
   )
 }
